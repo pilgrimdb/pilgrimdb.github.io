b3f855d734acab2d818d22f41d654f03f9781138
Peter Robins <github@peterrobins.co.uk>
2015-07-22 09:10:41 +0000

    Add routes to mapping

--- index.md
+++ index.md
@@ -3,3 +3,4 @@ title: Welcome
 ---
 
 * [Alphabetical list of routes](alphalist.html)
+* [Overview map of routes](maps.html?routes=)
--- js/setupMapdef.js
+++ js/setupMapdef.js
@@ -14,6 +14,11 @@ qs.forEach(function(part) {
     case 'feature':
       createMapdef(pair[1], '/features/');
       break;
+    case 'routes':
+      createMapdef('relief/[{"id":"Unmapped%20sections","url":"/simp.geojson"},'+
+        '{"id":"Routelines","url":"/unmapped.geojson","style":{"stroke":{"color":"rgba(255,255,0,0.6)","width":3,"lineDash":[5]}}}'+
+        ']');
+      break;
     default:
       el = document.createElement(pair[0]);
       el.innerHTML = pair[1];
@@ -23,6 +28,8 @@ qs.forEach(function(part) {
 
 document.body.appendChild(mapDef);
 
+// str - url string 'maptype/filename'
+// dir - optional dir to prefix to filename
 function createMapdef(str, dir) {
   var maptypes = {{site.data.maptypes | jsonify}};
   // what a horrible hack!
@@ -37,6 +44,7 @@ function createMapdef(str, dir) {
   el.innerHTML = "mouseposition,tooltip,featuredisplay";
   mapDef.appendChild(el);
   el = document.createElement('vectors');
-  el.innerHTML = dir + parts[1] + '.geojson';
+  // if dir, prepend and add geojson, else remove maptype/ from str
+  el.innerHTML = dir ? dir + parts[1] + '.geojson' : str.substring(str.indexOf('/')+1);
   mapDef.appendChild(el);
 }
